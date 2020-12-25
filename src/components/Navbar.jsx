a8f1eb7c41994de45ae883b88c68eb6131aa0132
Prince15130 <prince.bakshi2000@gmail.com>
2020-12-26 01:20:52 +0530

    Learning Context API

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { ThemeContext } from '../context/ThemeContext';
+
+class Navbar extends Component {
+    //static contextType = ThemeContext;
+    render() { 
+        
+        return (
+            <AuthContext.Consumer>{(authContext) =>(
+                <ThemeContext.Consumer>{(themeContext)=>{
+                    const{isLightTheme,light, dark} = themeContext;
+                    const theme = isLightTheme ? light : dark;
+                    const{isAuthenticated,toggleAuth} = authContext;
+                return(<nav style={{ background: theme.ui, color:theme.syntax}}>
+                    <h1>Context App</h1>
+                    <div onClick={toggleAuth}>
+                    {isAuthenticated ? "Logged In" : "Logged Out"}
+                    </div>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>);
+                }}
+                </ThemeContext.Consumer> 
+            )}
+            </AuthContext.Consumer>
+        );
+    }
+}
+ 
+export default Navbar;
